5585bbb332e6f5060d0cfbda09d9d207a6f4a689
Vipul Kumar <vipulkumar3885@gmail.com>
2026-04-09 11:41:19 +0530

    Fix: Pointing frontend to live Render backend

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,7 +1,3 @@
-const isProduction = import.meta.env.PROD; // Automatically true on Render
-
-export const API_BASE_URL = isProduction
-  ? "https://hospital-demo-2.onrender.com" // Live URL
-  : "http://localhost:8000";
+export const API_BASE_URL = "https://hospital-demo-2.onrender.com";
 export const MOCK_PATIENT_URL =
   "https://d4c5cce4-b1af-4f81-852b-edd97f9bf7e7.mock.pstmn.io/patients";
